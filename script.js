bcf38da0204cb8f4f85253b7a554fae512879bfc
veejaymax <104699738+veeejay25@users.noreply.github.com>
2025-01-07 17:11:41 +0000

    added functions for the rock paper scissors game

--- script.js
+++ script.js
@@ -1,1 +1,50 @@
-console.log("Hello World");
+let computerScore = 0;
+let humanScore = 0;
+
+function getComputerChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
+    const randomChoice = Math.floor(Math.random() * choices.length);
+    return choices[randomChoice];
+}
+
+function getHumanChoice() {
+    const humanChoice = prompt('Enter your choice: rock, paper, or scissors');
+    return humanChoice;
+    
+}
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        return 'It\'s a tie!';
+    }
+    else if (humanChoice === 'rock' && computerChoice === 'scissors' || humanChoice === 'paper' && computerChoice === 'rock' || humanChoice === 'scissors' && computerChoice === 'paper') {
+        humanScore++;
+        return 'You win!';
+    }
+    else {
+        computerScore++;
+        return 'You lose!';
+    }
+}
+
+function playGame() {
+    while (humanScore < 5 && computerScore < 5) {
+        const humanChoice = getHumanChoice();
+        const computerChoice = getComputerChoice();
+        console.log(playRound(humanChoice, computerChoice));
+        
+        console.log("Computer choice: " + computerChoice);
+        console.log("Human choice: " + humanChoice);
+        
+        console.log("Human score: " + humanScore);
+        console.log("Computer score: " + computerScore);
+    }
+
+    if (humanScore === 5) {
+        console.log("Congratulations! You won the game!");
+    } else {
+        console.log("Sorry, the computer won the game.");
+    }
+}
+
+playGame();
